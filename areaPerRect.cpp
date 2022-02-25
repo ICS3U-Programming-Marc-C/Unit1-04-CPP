d261995ecb20087c85b4b8dc7c18e3ccd4bddf1e
Ubuntu <ubuntu@ip-172-31-45-190.ec2.internal>
2022-02-25 02:03:03 +0000

    Pushing C++ files

---
+++ areaPerRect.cpp
@@ -0,0 +1,17 @@
+// Copyright (c) 2022 Coffi All rights reserved.
+
+
+// Created by: Marc Coffi
+
+// Date: Feb 17, 2022
+
+// This program calculates the area and perimeter of a rectangle.
+
+#include <iostream>
+#include <cmath>
+int main() {
+    std::cout << "If a rectangle has the dimensions:" <<std::endl;
+    std::cout << "6cm x 7cm"    << std::endl;
+    std::cout << "The area is " << (6*7) << "cm^2." << std::endl;
+    std::cout << "The perimeter is " << 2*(6+7) << "cm." << std::endl;
+}
---
+++ basicMath.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Coffi All rights reserved.
+
+
+// Created by: Marc Coffi
+
+// Date: Feb 17, 2022
+
+// This program do basic math
+
+#include <iostream>
+#include <cmath>
+int main() {
+    std::cout << "4+2=" << (4+2) << std::endl;
+    std::cout << "4-2=" << (4-2) << std::endl;
+    std::cout << "4/2=" << (4/2) << std::endl;
+    std::cout << "4+2*2=" << (4+2*2) << std::endl;
+    std::cout << "pow(4,2)=" << (pow(4, 2)) << std::endl;
+}
